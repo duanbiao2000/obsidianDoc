5e57be71c389ade23763e3c5187671a3b5b71459
danny <duanbiao2000@gmail.com>
2024-06-02 16:06:05 +0800

    vault backup: 2024-06-02 16:06:04

--- summary.py
+++ summary.py
@@ -3,7 +3,7 @@ import os
 # 设置你的Markdown文件所在的目录
 directory = "./"
 # 设置输出文件的路径
-output_file = "Summary.md"
+output_file = "SUMMARY.md"
 
 # 创建Markdown文件并写入基础内容
 with open(output_file, 'w',  encoding='utf-8') as md_file:
@@ -11,11 +11,11 @@ with open(output_file, 'w',  encoding='utf-8') as md_file:
     for chapter in os.listdir(directory):
         chapter_path = os.path.join(directory, chapter)
         if os.path.isdir(chapter_path):
-            md_file.write(f"## {chapter}\n")
+            md_file.write(f"* {chapter}\n")
             for file in os.listdir(chapter_path):
                 if file.endswith(".md"):
                     file_path = os.path.join(chapter, file)
-                    md_file.write(f"- [{file[:-3]}]({file_path})\n")
+                    md_file.write(f"* [{file[:-3]}]({file_path})\n")
 
 # 打印完成信息
 print(f"{output_file} has been created.")
